dfcccb72646d2ae4f16d3989d3709b039862a1ef
Alex Riviere <fimion@gmail.com>
2017-07-22 16:03:43 -0400

    started moving things and trying to get routing/login to work

--- alexpartyapi.py
+++ alexpartyapi.py
@@ -1,13 +1,38 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy import desc
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, \
+    RoleMixin, auth_token_required, login_required, current_user
+
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.config.from_pyfile('.env')
 
 db = SQLAlchemy(app)
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
 
 class Post(db.Model):
@@ -36,6 +61,18 @@ class Post(db.Model):
         return "Post: {} - {}".format(self.id, self.title)
 
 
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+# Create a user to test with
+# @app.before_first_request
+# def create_user():
+#     db.create_all()
+#     user_datastore.create_user(username='fimion', email='fimion@gmail.com', password='password')
+#     db.session.commit()
+
+
 @app.route('/')
 def hello_world():
     return jsonify('Hello World!')
@@ -44,6 +81,18 @@ def hello_world():
 @app.route('/posts')
 def get_posts():
     return jsonify(Post.page())
+
+
+@app.route('/_/posts/<id>', methods=['POST', 'PUT', 'DELETE'])
+@auth_token_required
+def edit_posts(id):
+    return jsonify(request.form)
+
+
+@app.route('/_/token', methods=['GET'])
+@login_required
+def get_token():
+    return jsonify(current_user.get_auth_token())
 
 if __name__ == '__main__':
     app.run()
